1cc4c99d1fb6e24b5093c82fbc73067f5bf8b60c
לאון זולוטריוב <kinglion1964@gmail.com>
2022-09-22 06:50:15 +0300

    Add project files.

---
+++ 6 HW/Program.cs
@@ -0,0 +1,22 @@
+﻿bool b = true;
+int[] array = new int[3];
+Console.WriteLine("Please choose 3 numbers to create a palindrome ");
+while (b)
+{
+    for (int i = 0; i < array.Length; i++) 
+    {
+        int.TryParse(Console.ReadLine(), out array[i]);
+    }
+    if (array[0] == array[2])
+    {
+        b = false;
+        Console.WriteLine("true");
+    }
+    else 
+    {
+        b = true;
+        Console.WriteLine("Try again");
+    }
+}
+
+
